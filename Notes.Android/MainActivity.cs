10950dbaa14541e68f47f76601e654dfce34aa43
YoungDi <u9951062@gmail.com>
2020-04-04 22:40:02 +0800

    init commit

---
+++ Notes.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Notes.Droid
+{
+    [Activity(Label = "Notes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private const string databaseName = "Notes.db";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            CopyDocuments();
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public static void CopyDocuments()
+        {
+            var dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), databaseName);
+            if (!File.Exists(dbPath))
+            {
+                using (var br = new BinaryReader(Application.Context.Assets.Open(databaseName)))
+                {
+                    using (var bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int length = 0;
+                        while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bw.Write(buffer, 0, length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Notes/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Notes.Data;
+
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Notes.db");
+                    var pathIsExist = File.Exists(path);
+                    Console.WriteLine(pathIsExist);
+                    Console.WriteLine("OOXX-222");
+
+                    database = new NoteDatabase(path);
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
